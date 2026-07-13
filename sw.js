afae9de002e51ddfadc486b8c534d0fdb406728e
arthabumi-id <arthabumi.id@gmail.com>
2026-07-13 19:30:06 +0700

    upd

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = 'fcc-arthabumi-v66';
+const CACHE = 'fcc-arthabumi-v67';
 const ASSETS = ['./index.html', './manifest.json', './icon-192.png', './icon-512.png', './apple-touch-icon.png'];
 
 self.addEventListener('install', e => {
